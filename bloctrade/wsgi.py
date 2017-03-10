df988f27a31297f06dbd3638fc43b8810b3a9381
PiyushW <piyushwankhade01@gmail.com>
2017-03-11 01:05:54 +0530

    kuu

--- bloctrade/wsgi.py
+++ bloctrade/wsgi.py
@@ -9,10 +9,9 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 import sys
-sys.path.append('/usr/bin/python')
 # sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api-name.settings")
-
+sys.path.append('/usr/local/lib/python2.7/site-packages')
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bloctrade.settings")
